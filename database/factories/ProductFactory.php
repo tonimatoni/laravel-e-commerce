0883417b50ec27fde0dc9f537303297ed871d29b
tonimatoni <matts.mata@gmail.com>
2026-01-19 14:39:53 +0100

    feat: random discounts in product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,10 +16,24 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $hasDiscount = fake()->boolean(30);
+        $discountPercentage = null;
+        $discountStartDate = null;
+        $discountEndDate = null;
+
+        if ($hasDiscount) {
+            $discountPercentage = fake()->randomFloat(2, 5, 50);
+            $discountStartDate = fake()->dateTimeBetween('-1 week', '+1 week');
+            $discountEndDate = fake()->dateTimeBetween($discountStartDate, '+2 months');
+        }
+
         return [
             'name' => fake()->words(3, true),
             'description' => fake()->paragraph(),
             'price' => fake()->randomFloat(2, 10, 1000),
+            'discount_percentage' => $discountPercentage,
+            'discount_start_date' => $discountStartDate,
+            'discount_end_date' => $discountEndDate,
             'stock_quantity' => fake()->numberBetween(0, 100),
             'sku' => fake()->unique()->bothify('SKU-####-???'),
             'image_url' => fake()->imageUrl(),
